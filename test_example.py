0d8fa7b73a40e6267c6a453cb9b3cf7a73f824cf
modhurita <modhurita@gmail.com>
2023-06-22 12:28:29 +0200

    add test for subtract

--- test_example.py
+++ test_example.py
@@ -1,7 +1,13 @@
-from example import add
+from example import add, subtract
 
 def test_add():
     assert add(0,0) == 0
     assert add(2,3) == 5
     assert add(-2,-3) == -5
     assert add(-2,3) == 1
+
+def test_subtract():
+    assert add(0,0) == 0
+    assert subtract(1,0) == 1
+    assert subtract(3,2) == 1
+    
